df7d3da46462c39603557ca4e1a2fabd5b1f7f07
azan <azanr7260@gmail.com>
2026-06-17 13:32:09 +0500

    Update permissions in Role.js

--- src/models/Role.js
+++ src/models/Role.js
@@ -1,21 +1,49 @@
 const mongoose = require("mongoose");
 
 const PERMISSIONS = [
+  "view_analytics",
+
+  "view_products",
+  "create_products",
+  "edit_products",
+  "delete_products",
+  "manage_products",
+
+  "view_orders",
+  "update_order_status",
+  "fulfill_orders",
+  "refund_orders",
+  "manage_orders",
+
+  "view_games",
+  "create_games",
+  "edit_games",
+  "delete_games",
   "manage_games",
   "manage_categories",
-  "manage_products",
-  "manage_orders",
-  "manage_claims",
-  "manage_team",
-  "manage_roles",
-  "view_analytics",
+
   "edit_site_content",
+  "upload_images",
+
   "manage_promos",
+
+  "view_claims",
+  "claim_agent",
   "monitor_agents",
   "view_pod",
-  "claim_agent",
+  "manage_claims",
+
+  "view_team",
+  "invite_team",
+  "edit_team",
+  "remove_team",
+  "manage_roles",
   "manage_collaborators",
-  "upload_images",
+  "manage_team",
+
+  "view_stock",
+  "manage_stock",
+  "manage_stockers",
 ];
 
 const roleSchema = new mongoose.Schema(
